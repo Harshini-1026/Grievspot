9d6608d5c6293be78c2a186da427513808b962f3
Harshini-1026 <harshinishakthivelu@gmail.com>
2025-04-28 15:29:27 +0530

    Update User.java

--- User.java
+++ User.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public abstract class User {
     protected String username, password, role;
 
@@ -15,4 +16,48 @@ public abstract class User {
     }
 
     public abstract void showMenu(GrievanceManager gm);
+}
+
+class Complainant extends User {
+
+    public Complainant(String username, String password) {
+        super(username, password, "Complainant");
+    }
+
+    @Override
+    public void showMenu(GrievanceManager gm) {
+        Scanner sc = new Scanner(System.in);
+        int choice;
+        do {
+            System.out.println("\n--- Complainant Menu ---");
+            System.out.println("1. Submit Grievance");
+            System.out.println("2. View My Grievances");
+            System.out.println("0. Logout");
+            System.out.print("Choice: ");
+            choice = sc.nextInt();
+            sc.nextLine(); // consume newline
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Department: ");
+                    String dept = sc.nextLine();
+                    System.out.print("Category: ");
+                    String cat = sc.nextLine();
+                    System.out.print("Description: ");
+                    String desc = sc.nextLine();
+                    Grievance g = new Grievance(username, dept, cat, desc);
+                    gm.addGrievance(g);
+                    System.out.println("Submitted with ID: " + g.getId());
+                    break;
+                case 2:
+                    gm.viewGrievancesByUser(username);
+                    break;
+                case 0:
+                    System.out.println("Logging out...");
+                    break;
+                default:
+                    System.out.println("Invalid choice.");
+            }
+        } while (choice != 0);
+    }
 }
